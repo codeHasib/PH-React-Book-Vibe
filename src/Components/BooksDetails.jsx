87e700dfaedd54dc5a72d10a7836b0306de23bc4
codeHasib <mohammadhasib9827@gmail.com>
2026-04-06 01:50:53 +0600

    add book details page with routing config

--- src/Components/BooksDetails.jsx
+++ src/Components/BooksDetails.jsx
@@ -1,9 +1,98 @@
-import React from "react";
-import { useParams } from "react-router";
+import { React, useContext } from "react";
+import { useNavigate, useParams } from "react-router";
+import { BookContext } from "../Context/BookContext";
 
 const BooksDetails = () => {
   const { id } = useParams();
-  return <div>This is bookId {id}</div>;
+  const { books } = useContext(BookContext);
+  const currentBook = books.find((b) => Number(id) === Number(b.bookId));
+
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
+  const {
+    bookName,
+    author,
+    image,
+    tags,
+    rating,
+    category,
+    review,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+  } = currentBook;
+
+  return (
+    <>
+      <div className="card lg:card-side bg-base-100 shadow-sm my-10 p-10">
+        <figure className="flex-1 flex justify-center items-center bg-base-300 py-50">
+          <img className="w-[50%]" src={image} alt={bookName} />
+        </figure>
+        <div className="card-body flex-1 ">
+          <h2 className="card-title"> {bookName} </h2>
+          <h2 className="my-5">
+            {" "}
+            By : <strong> {author} </strong>{" "}
+          </h2>
+          <div className="border-t border-b py-5">
+            <h3 className="text-xl">{category}</h3>
+          </div>
+          <div className="py-4 border-b">
+            <p>
+              <strong>Review : </strong> {review}
+            </p>
+            <div className="my-5">
+              <span>
+                {" "}
+                <strong> Tag : </strong>{" "}
+              </span>
+              {tags.map((tag, ind) => (
+                <>
+                  {" "}
+                  <div
+                    key={ind}
+                    className="badge text-green-500 bg-green-200 p-4 rounded-2xl"
+                  >
+                    {" "}
+                    {tag}{" "}
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="my-10">
+            <p>
+              Number of Pages: <strong>{totalPages}</strong>
+            </p>
+            <p>
+              Publisher: <strong>{publisher}</strong>
+            </p>
+            <p>
+              Year of Publishing: <strong>{yearOfPublishing}</strong>
+            </p>
+            <p className="mb-10">
+              Rating: <strong>{rating}</strong>
+            </p>
+            <button className="btn btn-ghost font-bold mr-5 border border-gray-200">
+              Read
+            </button>
+            <button className="btn btn-success text-white bg-sky-400 font-bold">
+              Wishlist
+            </button>
+            <div className="text-center my-10">
+              <button onClick={goBack} className="btn btn-error text-white">
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BooksDetails;
